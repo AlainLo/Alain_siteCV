c6b5211e1509a6aee2036c5fcdab3b4db3aac741
Alain LORTAL <alain.lortal@lepoles.com>
2017-10-06 13:07:37 +0200

    commit6
    update to index

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,10 @@
+<?php> require 'connexion.php'; ?>
 <!Doctype html>
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title> Admin : prénom nom </title>
 </head>
 <html>
---
+++ connexion.php
@@ -0,0 +1,10 @@
+<?php
+$hote='localhost'; // le chemin vers le serveur
+$bdd='alainlortal_cvsite'; // le nom de la base de données
+$utilisateur='root'; // le nom de l'utilisateur pour se connecter
+$passe='admin'; // le mot de passe local 
+
+$pdoCV = new PDO ('mysql:host=' . $hote . ';dbname='. $bdd, $utilisateur, $passe);
+//$pdoCV est le nom de la varaible de la connexion qui sort partout où l'on doit se servir de cette connexion
+$pdoCV->exec("SET NAMES utf8");
+?>
