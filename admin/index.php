216a8fb9e3f83ba4fa9883d00d9a1a2a6cf62646
Alain LORTAL <alain.lortal@lepoles.com>
2017-10-06 15:14:31 +0200

    commit9
    commit9

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,20 @@
 <?php require 'connexion.php'; ?>
+
 <!Doctype html>
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title> Admin : prénom nom </title>
+	<?php 
+		$sql = $pdoCV -> query("SELECT * FROM t_utilisateurs"); 
+		$ligne_utilisateur = $sql -> fetch(PDO::FETCH_ASSOC); 
+	?>
+	<title> Admin : <?= $ligne_utilisateur['prenom']; ?> <?= $ligne_utilisateur['nom']; ?></title>
 </head>
 <html>
 	<body>
-		<h1>Admin</h1>
+		<h1>Admin : <?= $ligne_utilisateur['prenom'].' '.$ligne_utilisateur['nom'] ?></h1>
 		
 	</body>
 </html>
