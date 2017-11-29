d50cb3fb537d1185a4f89be844a6f41daaf5af3e
AlainLo <alain.lortal@lepoles.com>
2017-11-29 11:54:34 +0100

    commit 19
    travail base frontend(suite)

--- index_public.php
+++ index_public.php
@@ -1,100 +1,60 @@
 <?php 
 $db = new PDO('mysql:host=localhost;dbname=alainlortal_cvsite;charset=utf8','root', ''); // connexion à MySQL
+$db->setAttribute(PDO::FETCH_ASSOC);
 $sql = 'SELECT * FROM t_utilisateurs';
-$req = $pdo->query($sql);
-while($row = $req->fetch()){
-	echo "Vous êtes sur le site de '.$row['prenom']' '.$row['nom']'";
-}
+$resultat = $pdo->query($sql);
+$resultat['nom'] . ' ' . $resultat['nom']; ?>
  
 
 
 ?>
-<!doctype html>+
+<!doctype html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		
-		<title> Site de  <?= $ligne_utilisateur['prenom']; ?> <?= $ligne_utilisateur['nom']; ?></title>
+		<title> Site de  <?= $resultat['prenom'] . ' ' . $resultat['nom']; ?></title>
 
 		<!-- Bootstrap -->
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <!--  <link href="css/bootstrap.min.css" rel="stylesheet"> -->
 		<!--personal css-->
 		<link href="css/stylepublic.css" rel="stylesheet">
 
-	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	    <!--[if lt IE 9]>
-	      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	    <![endif]-->
+	   
 	</head>
+    
+   
 	<body>
+        <header>
+         <h1> Site de  <?= $resultat['prenom']; ?> <?= $resultat['nom']; ?></h1>
+        </header>
+        <section>
+            utilisateur
+        </section>
 
-			<?php include('navbar.php');?>
-		<section>
-			<h1> site cv de <?php echo($ligne_utilisateur['prenom'].' '.$ligne_utilisateur['nom']); ?></h1>
-	      	
-	      	<div class="container">
-				<hr>
-				<?php
-						$sql = $pdoCV->prepare("SELECT * FROM t_realisations");
-						$sql->execute();
-						$nbr_realisations = $sql->rowCount();
-						//$ligne_realisation = $sql->fetch();
-				?>
-				<div class="row">
-			        <div class="col-md-8">
-			        	<h2> il y a <?php echo $nbr_realisations; ?> réalisations</h2>
-			            <table class="table  table-hover table-condensed">
-								<tr>
-									<th>titre</th>
-									<th>sous-titre</th>
-									<th>dates</th>	
-									<th>description</th>
-								</tr>
-								<tr>
-								<?php while ($ligne_realisation = $sql->fetch()){ ?>
-									<td><?php echo $ligne_realisation['r_titre']; ?></td>
-									<td><?php echo $ligne_realisation['r_soustitre']; ?></td>
-									<td><?php echo $ligne_realisation['r_dates']; ?></td>
-									<td><?php echo $ligne_realisation['r_description']; ?></td>
-								</tr>
-								<?php } ?>
-						</table>
-					</div>
-
-				</div>
-			</div>
-			<div class="container">
-				<?php
-						$sql = $pdoCV->prepare("SELECT * FROM t_formations");
-						$sql->execute();
-						$nbr_formations = $sql->rowCount();
-						//$ligne_formation = $sql->fetch();
-				?>
-				<div class="row">	
-			        	<div class="col-md-8">
-			        		<h2> il y a <?php echo $nbr_formations; ?> formations </h2>
-							<table class="table  table-hover table-condensed">
-								<tr>
-									<th>titre</th>
-									<th>sous-titre</th>
-									<th>dates</th>	
-									<th>description</th>
-								</tr>
-								<tr>
-									<?php while ($ligne_formation = $sql->fetch()){ ?>
-									<td><?php echo $ligne_formation['f_titre']; ?></td>
-									<td><?php echo $ligne_formation['f_soustitre']; ?></td>
-									<td><?php echo $ligne_formation['f_dates']; ?></td>
-									<td><?php echo $ligne_formation['f_description']; ?></td>
-								</tr>
-							</table>
-						</div>
-					</div>	
+        <section>
+            compétences
 		</section>	
+        
+        <section>
+            réalisations
+		</section>	
+        
+        <section>
+            formations
+		</section>	
+        
+        <section>
+            réseaux
+		</section>	
+        
+        <section>
+            loisirs
+		</section>	
+        
+        
 
 
 
